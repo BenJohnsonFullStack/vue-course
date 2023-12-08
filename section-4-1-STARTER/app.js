a1009b38ce4a024bb46934fc9a0f209d85deafd9
Ben Johnson <monk.sound.design@gmail.com>
2023-12-08 15:26:22 -0500

    lifecycle hooks

--- section-4-1-STARTER/app.js
+++ section-4-1-STARTER/app.js
@@ -4,6 +4,30 @@ let vm = Vue.createApp({
       message: "Hello world!",
     };
   },
+  beforeCreate() {
+    console.log("before create function called", this.message);
+  },
+  created() {
+    console.log("create function called", this.message);
+  },
+  beforeMount() {
+    console.log("before mount function called", this.$el);
+  },
+  mounted() {
+    console.log("mounted function called", this.$el);
+  },
+  beforeUpdate() {
+    console.log("before update function called");
+  },
+  updated() {
+    console.log("updated function called");
+  },
+  beforeUnmount() {
+    console.log("before unmount function called");
+  },
+  unmounted() {
+    console.log("unmounted function called");
+  },
 });
 
 vm.mount("#app");
